820cf52d9cde97407e72b48bafc85d9d9b463585
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2025-07-15 22:20:07 -0600

    Add: BuyIcon and its new design

--- src/components/CartComponent/Cart.jsx
+++ src/components/CartComponent/Cart.jsx
@@ -1,6 +1,7 @@
 import './Cart.css'
 import { useCart } from "../../hooks/useCart"
 import { CartProd } from '../CartProd/CartProd'
+import { BuyIcon } from '../icons/BuyIcon'
 
 export function Cart(){
     const {cartItems, isCartOpen, handleCloseCart, handleBuyCart} = useCart()
@@ -17,8 +18,10 @@ export function Cart(){
                         <CartProd key={game.id} gameCartList={game}/>
                     ))}
                 </section>
-                {cartItems.length > 0 && <section>
-                    <button onClick={() => handleBuyCart(cartItems)}>Buy</button>
+                {cartItems.length > 0 && <section className='buy-button-container'>
+                    <button className='buy-button' onClick={() => handleBuyCart(cartItems)}>
+                        Buy <BuyIcon color="white" size={20} />
+                    </button>
                 </section>}
             </div>
         </>
