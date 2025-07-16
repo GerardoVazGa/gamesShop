02fce286ee9d01ddc5772b0da60c10faa5dab127
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2025-07-16 11:22:45 -0600

    Add: total price in cart and overlay click close

--- src/components/CartComponent/Cart.jsx
+++ src/components/CartComponent/Cart.jsx
@@ -3,13 +3,24 @@ import { useCart } from "../../hooks/useCart"
 import { CartProd } from '../CartProd/CartProd'
 import { BuyIcon } from '../icons/BuyIcon'
 import { CloseIcon } from '../icons/CloseIcon'
+import { useRef } from 'react'
 
 export function Cart(){
     const {cartItems, isCartOpen, handleCloseCart, handleBuyCart} = useCart()
+    const sidebarBack = useRef(null)
 
     const sidebar = isCartOpen ? 'cart-container open' : 'cart-container'
+
+    const totalPrice = cartItems.reduce((acc, item) => acc + (item.price * item.amount), 0).toFixed(2)
+
+    const handleClickOutside = (e) => {
+        if (!sidebarBack.current) return
+        if (sidebarBack.current === e.target) {
+            handleCloseCart()
+        }
+    }
     return(
-        <>
+        <div className={isCartOpen ? 'cart' : ""} ref={sidebarBack} onClick={handleClickOutside}>
             <div className={sidebar}>
                 <section className='button-close'>
                     <button onClick={handleCloseCart}>
@@ -22,11 +33,14 @@ export function Cart(){
                     ))}
                 </section>
                 {cartItems.length > 0 && <section className='buy-button-container'>
+                    <div className='total-price'>
+                        <p>Total: ${totalPrice}</p>
+                    </div>
                     <button className='buy-button' onClick={() => handleBuyCart(cartItems)}>
                         Buy <BuyIcon color="white" size={20} />
                     </button>
                 </section>}
             </div>
-        </>
+        </div>
     )
 }
